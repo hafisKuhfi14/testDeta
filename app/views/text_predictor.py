6864d4abc6e0ed450b51dfb5ac71c4db50c9ff52
dhino12 <dhinorahmad0@gmail.com>
2023-08-02 00:30:50 +0700

    fix nb text_polarity

--- app/views/text_predictor.py
+++ app/views/text_predictor.py
@@ -168,7 +168,8 @@ def text_preprocessing(df):
         st.markdown(kotak_berwarna("#c2c2c27a", f"Shortword: {df['Shortword'][0]}"), unsafe_allow_html=True)
         df_labeling = {
                 "split_word": df['Text_Clean_split'],
-                "polarity": df['polarity']
+                "polarity": df['polarity'],
+                "bobot": df['polarity_score']
         }
         st.dataframe(df_labeling, use_container_width=True)
         # st.dataframe(df['Text_Clean'].head(20), use_container_width=True)
